ade5b0f15e392f122d0a3456d0844fb7aeab6fe3
OSTWAL MUSKAN <129897455+Muskanostwal@users.noreply.github.com>
2025-07-12 22:49:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const productRouter = require("./routes/product");
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/ProductDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.log(err));
+
+app.use(express.json());
+app.use("/product", productRouter);
+
+app.listen(8080, () => {
+    console.log("Server running on http://localhost:8080");
+});
